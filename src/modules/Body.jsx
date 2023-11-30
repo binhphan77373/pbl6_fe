392d668f3eca9c05a91ec39dc3e26130e06f9505
Phan Bình <74055586+binhphan77373@users.noreply.github.com>
2023-11-30 16:47:50 +0700

    Update Body.jsx
    Second Commit

--- src/modules/Body.jsx
+++ src/modules/Body.jsx
@@ -6,6 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 const Body = () => {
   const [file, setFile] = useState(null);
   const [image, setImage] = useState(null);
+  const [text,setText] = useState(null);
   const [process, setProcess] = useState(0);
   const [rs, setRs] = useState("");
   const navigate = useNavigate();
@@ -40,7 +41,7 @@ const Body = () => {
     // Create a FormData object and append the image to it.
     const formData = new FormData();
     formData.append('image_input', image);
-    formData.append('text_input', "cmm");
+    formData.append('text_input', text);
     console.log(image);
     console.log(formData);
     // Make a POST request to the server with the FormData object.
@@ -59,13 +60,16 @@ const Body = () => {
       // setNewImage(src);
       console.log("success");
       setRs(response.data);
-      navigate('/rs', { state: { result: response.data } });
+      navigate('/rs', { state: { result: response.data , resultImg : image } });
     } else {
       // An error occurred.
       // Handle the error.
       console.error('Failed to upload and process the image.');
       console.log(image);
     }
+  }
+  const handleChange = (e) => {
+    setText(e.target.value);
   }
   return (
     <div className="flex flex-col gap-5 justify-center items-center">
@@ -81,6 +85,7 @@ const Body = () => {
             <textarea
               className="peer h-full border-t-white text-white min-h-[100px] w-full resize-none rounded-[7px] border border-white border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-green-400 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
               placeholder=" "
+              onChange={handleChange}
             ></textarea>
             <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-white peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-green-400 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-green-400 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
               <p className="text-white">Description</p>
